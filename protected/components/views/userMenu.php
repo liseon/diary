5aa8d7e159ae37fbd41bfc894a339480f83451dd
Igor <ik.pr@bk.ru>
2013-09-21 22:04:51 +0400

    UserMenu 2

---
+++ protected/components/views/userMenu.php
@@ -0,0 +1,5 @@
+<ul>
+    <li><?php echo CHtml::link('Список причин',array('reasons/index')); ?></li>
+    <li><?php echo CHtml::link('Список действий',array('doings/index')); ?></li>
+    <li><?php echo CHtml::link('Выход',array('site/logout')); ?></li>
+</ul>
